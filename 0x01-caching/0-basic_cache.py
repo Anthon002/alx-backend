298e620dfcffc60f1c266c1f307147a0fcafe35e
anthon002 <chineduanulugwo@gmail.com>
2024-04-03 19:54:24 +0000

    putting and getting in dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#usr/bin/python3
+from base_caching import BaseCaching
+"""Task Basic dictionary
+"""
+
+
+class BasicCache(BaseCaching):
+    """ class to put and retrieve info from a dictionary
+    """
+    def put(self, key, item):
+        """method to save into item
+        """
+        if(key is None or item is None):
+            return 
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if (key is None):
+            return 
+        _item = self.cache_data.get(key, None)
+        return _item

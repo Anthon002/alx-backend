e802f515a9db2ec69e626ec1cbddf4fad143f861
Anthon002 <chineduanulugwo@gmail.com>
2024-04-09 21:40:34 +0100

    configuring babbel task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 """ module for task 1
 """
+from flask_babel import Babel
 from flask import Flask, render_template
-from flask_bable import Babel
 
 
 class Config:
-    """Class for Bable configuration
+    """Class for configuring babbel.
     """
-    LANGUAGES = ["en","fr"]
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @app.route('/')
 def get_index():
-    """ method for index page
+    """method for the index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

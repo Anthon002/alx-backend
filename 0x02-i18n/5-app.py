020fd29af83e3e6e126bc163a7f87ccc76da9b94
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-04-12 18:44:06 +0100

    task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" module for task 5.
+"""
+from typing import Union, Dict
+from flask_babel import Babel
+from flask import render_template,Flask, request, g
+
+
+class Config:
+    """ Class for configuring flask Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """module to get a user using user id.
+    """
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """ method to do routines before each request is complete.
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ method to get web page locale.
+    """
+    web_locale = request.args.get('locale', '')
+    if web_locale in app.config["LANGUAGES"]:
+        return web_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """method for the index page.
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

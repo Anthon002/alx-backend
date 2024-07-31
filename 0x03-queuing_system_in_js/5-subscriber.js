d925a885f4201b08e1ecf2605fab158fcda33d42
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-31 06:52:21 +0100

    task 5

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,23 +1,23 @@
 #!/usr/bin/yarn dev
 import { createClient } from 'redis';
 
-const client = createClient();
-const EXIT_MSG = 'KILL_SERVER';
+var clientObj = createClient();
+var EXIT_MSG = 'KILL_SERVER';
 
-client.on('error', (err) => {
+clientObj.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
 });
 
-client.on('connect', () => {
+clientObj.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-client.subscribe('holberton school channel');
+clientObj.subscribe('holberton school channel');
 
-client.on('message', (_err, msg) => {
+clientObj.on('message', (_err, msg) => {
   console.log(msg);
   if (msg === EXIT_MSG) {
-    client.unsubscribe();
-    client.quit();
+    clientObj.unsubscribe();
+    clientObj.quit();
   }
 });

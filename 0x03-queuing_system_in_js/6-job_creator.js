3c612d59c3db6ab62760f7e3837e09c0b23252b4
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-31 06:54:03 +0100

    task 6

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,13 +1,11 @@
 #!/usr/bin/yarn dev
 import { createQueue } from 'kue';
 
-const queue = createQueue({name: 'push_notification_code'});
-
-const job = queue.create('push_notification_code', {
+var queue = createQueue({name: 'push_notification_code'});
+var job = queue.create('push_notification_code', {
   phoneNumber: '07045679939',
   message: 'Account registered',
 });
-
 job
   .on('enqueue', () => {
     console.log('Notification job created:', job.id);
